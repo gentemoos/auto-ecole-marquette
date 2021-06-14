a63321d97ab6152c1c83d5a88095aef742ec1931
gentemoos <65900921+gentemoos@users.noreply.github.com>
2021-06-14 23:15:19 +0200

    Add files via upload

---
+++ js/script_animation.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    // animation des cartes formations
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = { root: document.querySelector('#someSpecificParent')};
+  
+  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+  ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+  });
+
+
+
+
+})
